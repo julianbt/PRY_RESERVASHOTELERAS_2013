3aa02942be5db9d31047efbb47edb333e9386475
xkiRox <zkiroz@gmail.com>
2013-11-22 02:07:00 -0500

    Modificacion Theme

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,7 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Hotel Decamesoft',
 		
-	'theme'=>'decamesoft',
+	#'theme'=>'decamesoft',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
